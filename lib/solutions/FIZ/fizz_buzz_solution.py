c4d1047b3283ccdbae374f91b1d67cce63e25d39
sbx_user1059 <sbx_user1059@ip-10-128-181-242.eu-west-2.compute.internal>
2018-11-25 17:35:38 +0000

    Sun Nov 25 17:35:38 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -5,10 +5,18 @@ def fizz_buzz(number):
         if number > 10 and (str(number) == len(str(number)) * str(number)[0]):
             return "fizz buzz deluxe"
         return "fizz buzz"
+
+    if number % 3 == 0 or '3' in str(number):
+        if number > 10 and (str(number) == len(str(number)) * str(number)[0]):
+            return "fizz deluxe"
+        return "fizz"
+
+    if number % 5 == 0 or '5' in str(number):
+        if number > 10 and (str(number) == len(str(number)) * str(number)[0]):
+            return "buzz deluxe"
+        return "buzz"
+
     if number > 10 and (str(number) == len(str(number)) * str(number)[0]):
         return "deluxe"
-    if number % 3 == 0 or '3' in str(number):
-        return "fizz"
-    if number % 5 == 0 or '5' in str(number):
-        return "buzz"
+
     return str(number)
