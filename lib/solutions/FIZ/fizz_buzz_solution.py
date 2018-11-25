276bccea0ffb6dbc3754a70d5792d958c8242673
sbx_user1059 <sbx_user1059@ip-10-128-181-242.eu-west-2.compute.internal>
2018-11-25 17:29:11 +0000

    Sun Nov 25 17:29:11 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,6 +1,9 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
+
     if (number % 3 == 0 or '3' in str(number)) and (number % 5 == 0 or '5' in str(number)):
+        if (str(number) == len(str(number)) * str(number)[0]):
+            return "fizz buzz deluxe"
         return "fizz buzz"
     if number % 3 == 0 or '3' in str(number):
         return "fizz"
--- test/solution_tests/FIZ/test_fiz.py
+++ test/solution_tests/FIZ/test_fiz.py
@@ -6,11 +6,13 @@ class TestFiz(unittest.TestCase):
     def test_fiz(self):
         self.assertEqual(fizz_buzz_solution.fizz_buzz(1), "1")
         self.assertEqual(fizz_buzz_solution.fizz_buzz(3), "fizz")
+        #self.assertEqual(fizz_buzz_solution.fizz_buzz(11), "deluxe")
         self.assertEqual(fizz_buzz_solution.fizz_buzz(13), "fizz")
         self.assertEqual(fizz_buzz_solution.fizz_buzz(5), "buzz")
         self.assertEqual(fizz_buzz_solution.fizz_buzz(25), "buzz")
         self.assertEqual(fizz_buzz_solution.fizz_buzz(35), "fizz buzz")
         self.assertEqual(fizz_buzz_solution.fizz_buzz(15), "fizz buzz")
+        self.assertEqual(fizz_buzz_solution.fizz_buzz(555), "fizz buzz deluxe")
 
     if __name__ == '__main__':
         unittest.main()
