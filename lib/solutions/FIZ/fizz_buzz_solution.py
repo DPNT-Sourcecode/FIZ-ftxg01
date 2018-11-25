df62da3809bfac24a8cf171d195bfaaaa553d168
sbx_user1059 <sbx_user1059@ip-10-128-181-242.eu-west-2.compute.internal>
2018-11-25 17:44:10 +0000

    Sun Nov 25 17:44:10 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -2,21 +2,31 @@
 def fizz_buzz(number):
 
     if (number % 3 == 0 or '3' in str(number)) and (number % 5 == 0 or '5' in str(number)):
-        if number > 10 and (str(number) == len(str(number)) * str(number)[0]):
-            return "fizz buzz deluxe"
+        delux = deluxe(number)
+        if delux:
+            return "fizz buzz " + delux
         return "fizz buzz"
 
     if number % 3 == 0 or '3' in str(number):
-        if number > 10 and (str(number) == len(str(number)) * str(number)[0]):
-            return "fizz deluxe"
+        delux = deluxe(number)
+        if delux:
+            return "fizz " + delux
         return "fizz"
 
     if number % 5 == 0 or '5' in str(number):
-        if number > 10 and (str(number) == len(str(number)) * str(number)[0]):
-            return "buzz deluxe"
+        delux = deluxe(number)
+        if delux:
+            return "buzz " + delux
         return "buzz"
 
-    if number > 10 and (str(number) == len(str(number)) * str(number)[0]):
-        return "deluxe"
+    delux = deluxe(number)
+    if delux:
+        return delux
 
     return str(number)
+
+def deluxe(number):
+    if number > 10 and (str(number) == len(str(number)) * str(number)[0]):
+        if number % 2 == 0:
+            return "fake deluxe"
+        return "deluxe"
--- test/solution_tests/FIZ/test_fiz.py
+++ test/solution_tests/FIZ/test_fiz.py
@@ -7,6 +7,7 @@ class TestFiz(unittest.TestCase):
         self.assertEqual(fizz_buzz_solution.fizz_buzz(1), "1")
         self.assertEqual(fizz_buzz_solution.fizz_buzz(3), "fizz")
         self.assertEqual(fizz_buzz_solution.fizz_buzz(11), "deluxe")
+        self.assertEqual(fizz_buzz_solution.fizz_buzz(22), "fake deluxe")
         self.assertEqual(fizz_buzz_solution.fizz_buzz(13), "fizz")
         self.assertEqual(fizz_buzz_solution.fizz_buzz(5), "buzz")
         self.assertEqual(fizz_buzz_solution.fizz_buzz(25), "buzz")
