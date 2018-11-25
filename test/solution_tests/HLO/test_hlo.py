f3dd61058e2d0a079a76656aaae957144f55c351
sbx_user1059 <sbx_user1059@ip-10-128-181-242.eu-west-2.compute.internal>
2018-11-25 16:55:20 +0000

    Sun Nov 25 16:55:20 UTC 2018

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -4,7 +4,7 @@ from lib.solutions.HLO import hello_solution
 
 class TestHello(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello_solution.hello("hey"), "Hello World")
+        self.assertEqual(hello_solution.hello("John"), "Hello, John!")
 
     if __name__ == '__main__':
         unittest.main()
