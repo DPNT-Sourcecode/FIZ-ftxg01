400c11d9200534780b4571da9685fffad6c79c57
sbx_user1059 <sbx_user1059@ip-10-128-181-242.eu-west-2.compute.internal>
2018-11-25 16:58:23 +0000

    Sun Nov 25 16:58:23 UTC 2018

---
+++ test/solution_tests/FIZ/test_fiz.py
@@ -0,0 +1,10 @@
+import unittest
+
+from lib.solutions.FIZ import fizz_buzz_solution
+
+class TestFiz(unittest.TestCase):
+    def test_fiz(self):
+      
+
+    if __name__ == '__main__':
+        unittest.main()
