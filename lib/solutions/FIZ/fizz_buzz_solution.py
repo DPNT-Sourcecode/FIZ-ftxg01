2640ba18223d2e569c7a11705370f36fd99676f5
sbx_user1059 <sbx_user1059@ip-10-128-181-242.eu-west-2.compute.internal>
2018-11-25 17:15:54 +0000

    Sun Nov 25 17:15:54 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,9 +1,9 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    if number % 3 == 0 and number % 5 == 0:
+    if (number % 3 == 0 or 3 in str(number)) and (number % 5 == 0 or 5 in str(number)):
         return "fizz buzz"
-    if number % 3 == 0:
+    if number % 3 == 0 or 3 in str(number):
         return "fizz"
-    if number % 5 == 0:
+    if number % 5 == 0 or 5 in str(number):
         return "buzz"
     return str(number)
