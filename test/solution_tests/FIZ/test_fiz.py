6a19e1ebaf8d8864313e5ddf69ac5f6f622d2b08
sbx_user1059 <sbx_user1059@ip-10-128-181-242.eu-west-2.compute.internal>
2018-11-25 17:05:00 +0000

    Sun Nov 25 17:05:00 UTC 2018

--- test/solution_tests/FIZ/test_fiz.py
+++ test/solution_tests/FIZ/test_fiz.py
@@ -4,7 +4,10 @@ from lib.solutions.FIZ import fizz_buzz_solution
 
 class TestFiz(unittest.TestCase):
     def test_fiz(self):
-      
-
+        self.assertEqual(fizz_buzz_solution.fizz_buzz(1), "1")
+        self.assertEqual(fizz_buzz_solution.fizz_buzz(3), "fizz")
+        self.assertEqual(fizz_buzz_solution.fizz_buzz(5), "buzz")
+        self.assertEqual(fizz_buzz_solution.fizz_buzz(15), "fizzbuzz")
+        
     if __name__ == '__main__':
         unittest.main()
